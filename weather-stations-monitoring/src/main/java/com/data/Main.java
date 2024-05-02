891d1a0b2f5352e3489dc1f6a0e6b006c82119ef
= <=>
2024-05-02 14:54:14 +0300

    basic producer with docker compose and Dokerfile configured

--- weather-stations-monitoring/src/main/java/com/data/Main.java
+++ weather-stations-monitoring/src/main/java/com/data/Main.java
@@ -1,7 +1,28 @@
 package com.data;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+import static com.data.constants.KafkaProps.*;
+import static com.data.constants.KafkaProps.TOPIC;
+
 public class Main {
+    private static Properties getProperties() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", BOOTSTRAP_SERVERS);
+        props.put("key.serializer", KEY_SERIALIZER);
+        props.put("value.serializer", VALUE_SERIALIZER);
+        return props;
+    }
+
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        Properties props = getProperties();
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+        String message = "hi ahmed this is kafka test from main";
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC, message);
+        producer.send(record);
+        producer.close();
     }
 }
---
+++ weather-stations-monitoring/src/main/java/com/data/status_messages/MessageTemp.java
@@ -0,0 +1,4 @@
+package com.data.status_messages;
+
+public class MessageTemp {
+}
